8f116d6275c29138299962a5ab96a4d5dd4cb5b8
Faiyaaz <faiyaaz.haque90@gmail.com>
2022-10-15 07:16:40 -0400

    got todos to add to list need to work on checkbox

--- main.js
+++ main.js
@@ -7,13 +7,45 @@ const addbutton = document.getElementById("add")
 addbutton.addEventListener('click', (e) => {
     e.preventDefault()
     const content = document.getElementById("todocontent").value;
-    console.log(content);
-        
+    todos.push(content);
+    rendertodos(todos) 
+    content.value = ""   
 })
 
 
 // render todos
 
 function rendertodos(list) {
-    
+    const todoContainer = document.getElementById('todo-list')
+    todoContainer.innerHTML = ""
+    list.forEach(todo => {
+        const todolist = document.getElementById('todo-list')
+        const todocase = document.createElement('div')
+        todocase.classList.add('todo')
+        todocase.innerText = todo
+        todolist.appendChild(todocase)
+        
+        const checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        checkbox.id = 'toggle'
+        todocase.appendChild(checkbox)
+
+        
+
+        const editButton = document.createElement('button')
+        editButton.classList.add('button')
+        editButton.innerText = "Edit Todo"
+        editButton.id = 'edit';
+        todocase.appendChild(editButton);
+
+        const deleteButton = document.createElement('button')
+        deleteButton.classList.add('button')
+        deleteButton.innerText = "Delete Todo"
+        deleteButton.id = 'delete';
+        todocase.appendChild(deleteButton);
+        
+
+
+    });
 }
+
